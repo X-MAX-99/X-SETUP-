e3340db1b1437deaa678f2db7a5c6db8373d7184
X-MAX-99 <123096410+X-MAX-99@users.noreply.github.com>
2023-01-21 09:38:36 +0600

    Add files via upload

---
+++ X-SETUP.py
@@ -0,0 +1,111 @@
+import os,sys
+os.system('clear')
+def install():
+  print("\033[1;32m")
+  os.system("pkg update -y")
+  print("\033[1;32m")
+  os.system("pkg upgrade -y")
+  print("\033[1;32m")
+  os.system("pkg install git -y")
+  print("\033[1;32m")
+  os.system("pkg install python -y")
+  print("\033[1;32m")
+  os.system("pkg install python2 -y")
+  print("\033[1;32m")
+  os.system("pkg install wget -y")
+  print("\033[1;32m")
+  os.system("pkg install figlet -y")
+  print("\033[1;32m")
+  os.system("pkg install curl -y")
+  print("\033[1;32m")
+  os.system("pkg install php -y")
+  print("\033[1;32m")
+  os.system("pkg install cmatrix -y")
+  print("\033[1;32m")
+  os.system("pkg install sl -y")
+  print("\033[1;32m")
+  os.system("pkg install ruby ")
+  print("\033[1;32m")
+  os.system("gem install lolcat -y")
+  print("\033[1;32m")
+  os.system("pkg install zip -y")
+  print("\033[1;32m")
+  os.system("pkg install unzip -y")
+  print("\033[1;32m")
+  os.system("pkg install openssh -y")
+  print("\033[1;32m")
+  os.system("pkg install clang -y")
+  print("\033[1;32m")
+  os.system("pkg install crunch -y")
+  print("\033[1;32m")
+  os.system("pkg install proot-distro -y")
+  print("\033[1;32m")
+  os.system("pkg install ncurses-utils -y")
+  print("\033[1;32m")
+  os.system("pip install --upgrade pip")
+  print("\033[1;32m")
+  os.system("pip install --upgrade pip")
+  print("\033[1;32m")
+  os.system("pip install requests")
+  print("\033[1;32m")
+  os.system("pip2 install requests")
+  print("\033[1;32m")
+  os.system("pip install rich")
+  print("\033[1;32m")
+  os.system("pip2 install rich")
+  print("\033[1;32m")
+  os.system("pip install bs4")
+  print("\033[1;32m")
+  os.system("pip2 install bs4")
+  print("\033[1;32m")
+  os.system("pip install mechanize")
+  print("\033[1;32m")
+  os.system("pip2 install mechanize")
+ab=('YOUR TERMUX SETUP IS SUCCESSFULLY DONE')
+bch=('''  
+\033[1;36mY88b   d88P        888b     d888         d8888  Y88b   d88P 
+ Y88b d88P         8888b   d8888        d88888   Y88b d88P  
+  Y88o88P          88888b.d88888       d88P888    Y88o88P   
+   Y888P           888Y88888P888      d88P 888     Y888P    
+   \033[1;35md888bb 88888888 888 Y888P 888     d88P  888     d888b    
+  d88888b          888  Y8P  888    d88P   888    d88888b   
+ d88P Y88b         888       888   d8888888888   d88P Y88b  
+d88P   Y88b        888       888  d88P     888  d88P   Y88b
+  
+
+
+*******************************************
+\033[1;31m*     [✓]  AUTHOR    :  \033[1;32m  X-MAX99          *
+\033[1;33m*     [✓]  NUMBER    :   \033[1;34m 01733782883     *
+\033[1;35m*     [✓]  WATSAPP   :   \033[1;36m 01733782883     *
+\033[1;37m*     [✓]  STATUS    :  \033[1;31m  FREE            *
+\033[1;32m*     [✓]  TOOLS     :   \033[1;33m TERMUX SETUP    *
+\033[1;34m*     [✓]  VERSION   :    \033[1;35m0.01            *
+*******************************************
+
+''')
+print(bch)
+klm=('\033[1;35mOPTIONS')
+uss=(f'\033[1;33m\t\tWELLCOME TO OUR TOOL\n\n\n\033[0m  \n\n\t\t{klm}')
+print(uss)
+kll=('\033[1;31m[2] EXIT')
+xml=(f'\n\t\033[1;32m[1] TERMUX FULL SETUP   \n \t{kll} ')
+print(xml)
+xyz=input('\n\t\033[1;35mENTER YOUR CHOISE :')
+print(xyz)
+if xyz=='1' or xyz=='01':
+  print('START INSTALLATION...') 
+  install()
+  print(ab)
+
+elif ab=='2' or ab=='02':
+	print('\033[1;31m\t\t*STOPED BY YOUR WISH*\033[0m')
+	sys.exit()
+else:
+	print('\033[1;31m*YOU ENTERED WRONG 	OPTION*\033[0m')
+	sys.exit()
+
+
+
+
+
